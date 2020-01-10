75f96545bc0a4c7ba16556060b393817930712ea
soja <jamshidi@cs.uoregon.edu>
2020-01-10 13:40:42 -0800

    added python courses

---
+++ Python/Session-1.py
@@ -0,0 +1,32 @@
+print('\n============  Output ===========')
+
+# condition  if-else
+
+a = int(input('please enter A:'))
+b = int(input('please enter B:'))
+c = input('enter the operator:')
+
+if c == '+':  # b == 0   b<0 b>0  b<=0 b>=0 b!=0  
+  print('A+B is: {} '.format(a+b) )
+else:
+  print('A-B is: {} '.format(a-b) )
+
+
+# a = 1 # integer
+# a = 2
+# a = 3
+# a = 1.5 # float
+# a = 'SOJA.ir' # string
+# a = 'S' # character - char
+
+#  ctrl + /   ---> to commend out a line
+# '100' 100
+
+# int --> str
+# 100 --> '100'
+# str(100) --> '100'
+
+# str --> int
+# '100' --> 100
+# int('100') --> 100
+
