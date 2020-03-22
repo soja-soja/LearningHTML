294df32adb3b3ce8027fd1f4719f6794d5aabe19
soja-soja <itsoja@gmail.com>
2020-03-22 16:14:51 -0700

    ASP-session2

---
+++ ASP.net/session2/soja.ir/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace soja.ir
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            txtUserName.Attributes.Add("placeholder", "نام کاربری را وارد کنید");
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            Label1.Text = txtUserName.Text;
+
+
+        }
+    }
+}
